a3217189c440358b27ae3d80e31f75f9097e8179
TCGlearning <aishwarya.salokhe@thecomfortgroup.co>
2025-05-02 11:23:10 +1200

    Add files via upload

---
+++ tc-config.js
@@ -0,0 +1,20 @@
+/* Tin Can configuration */
+
+//
+// ActivityID that is sent for the statement's object
+//
+TC_COURSE_ID = "http://_BtMlv3hteArFQpPhfMbMtV17PSr8XTS_rise"
+
+//
+// CourseName for the activity
+//
+TC_COURSE_NAME = {
+  "en-US": "Flexirest@BRU (In Progress - April 2025)"
+};
+
+//
+// CourseDesc for the activity
+//
+TC_COURSE_DESC = {
+  "en-US": "&lt;div data-editor-id=&#34;cbf903c9-4aa1-4123-af78-044cbee44202&#34;&gt;&lt;p&gt;Introduce Dream Tokens = Benefits that help your customer sleep better&lt;/p&gt;&lt;/div&gt;"
+};
